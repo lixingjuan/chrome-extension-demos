4091c9eb4cb9511b1d3de0577ab6cfdc7924f411
xingjuan.li <xingjuan.li@datayes.com>
2023-06-30 17:47:19 +0800

    feat: body-editable, 支持body可编辑

---
+++ body-editable/README.md
@@ -0,0 +1,3 @@
+# body-editable
+
+支持document 可编辑
---
+++ body-editable/background.js
@@ -0,0 +1,33 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "OFF",
+  });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  // Next state will always be the opposite
+  const nextState = prevState === "ON" ? "OFF" : "ON";
+
+   await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+
+  if (nextState === "ON") {
+     await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: ()=>{
+        document.body.contentEditable = true
+      }
+    });
+  } else if (nextState === "OFF") {
+     await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: ()=>{
+        document.body.contentEditable = false
+      }
+    });
+  }
+});
